60750efeed37bad47a95c5665153ac814bc825d2
karrrakotov <karrrakotov@mail.ru>
2020-12-25 11:53:22 +0300

    Исправил ошабки

---
+++ ----.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import math
+
+
+#   Создать класс Triangle для представления треугольника. Поля данных должны включать
+#   углы и стороны. Требуется реализовать
+#   операции: получения полей данных,
+#   вычисления площади, вычисления периметра, вычисления высот, а также определения
+#   вида треугольника (равносторонний, равнобедренный или прямоугольный).
+
+class Triangle:
+
+    def __init__(self, side=0):
+        side = float(side)
+        self.__first = side
+        self.__second = side
+        self.__third = side
+
+    @property
+    def first(self):
+        return self.__first
+
+    @property
+    def second(self):
+        return self.__second
+
+    @property
+    def third(self):
+        return self.__third
+
+    # Ввод сторон треугольника
+    def read(self, prompt=None):
+        line = input() if prompt is None else input(prompt)
+        parts = list(map(float, line.split()))
+        if len(parts) != 3:
+            print("Неверный размер списка", file=sys.stderr)
+            exit(1)
+
+        if parts[0] == 0 or parts[1] == 0 or parts[2] == 0:
+            raise ValueError()
+
+        if parts[0] < 1 or parts[1] < 1 or parts[2] < 1:
+            raise ValueError()
+
+        self.__first = abs(parts[0])
+        self.__second = abs(parts[1])
+        self.__third = abs(parts[2])
+
+    # Вывести числа на экран
+    def display(self):
+        print(f"{self.__first}")
+
+    # Вычисление периметра треугольника
+    def add(self, rhs):
+        if isinstance(rhs, Triangle):
+            self.perimeter = self.first + self.second + self.third
+            print(f"Периметр треугольника равен: ")
+
+            return Triangle(self.perimeter)
+        else:
+            raise ValueError()
+
+    # Вычисление площади треугольника
+    def sub(self, rhs):
+        if isinstance(rhs, Triangle):
+            p = self.perimeter / 2
+            square = math.sqrt(p * (p - self.first) * (p - self.second) * (p - self.third))
+            print(f"Площадь треугольника равна: ")
+
+            return Triangle(square)
+        else:
+            raise ValueError()
+
+    # Вычисление высоты проведенной к стороне A
+    def height_one(self, rhs):
+        if isinstance(rhs, Triangle):
+            p = self.perimeter / 2
+            h1 = 2 * math.sqrt(p * (p - self.first) * (p - self.second) * (p - self.third)) / self.first
+            print(f"Высота  проведенная к стороне A равна: ")
+
+            return Triangle(h1)
+        else:
+            raise ValueError()
+
+    # Вычисление высоты проведенной к стороне B
+    def height_two(self, rhs):
+        if isinstance(rhs, Triangle):
+            p = self.perimeter / 2
+            h2 = 2 * math.sqrt(
+                p * (p - self.first) * (p - self.second) * (p - self.third)) / self.second
+            print(f"Высота  проведенная к стороне B равна: ")
+
+            return Triangle(h2)
+        else:
+            raise ValueError()
+
+    # Вычисление высоты проведенной к стороне C
+    def height_three(self, rhs):
+        if isinstance(rhs, Triangle):
+            p = self.perimeter / 2
+            h3 = 2 * math.sqrt(
+                p * (p - self.first) * (p - self.second) * (p - self.third)) / self.third
+            print(f"Высота  проведенная к стороне C равна: ")
+
+            return Triangle(h3)
+        else:
+            raise ValueError()
+
+    # Вычисление градусов углов
+    def corner_one(self, rhs):
+        if isinstance(rhs, Triangle):
+            a = self.first
+            b = self.second
+            c = self.third
+            first_corner = math.acos(((b ** 2) + (c ** 2) - (a ** 2)) / (2 * c * b))
+            first_degrees = math.degrees(first_corner)
+
+            second_corner = math.acos(((a ** 2) + (b ** 2) - (c ** 2)) / (2 * a * b))
+            second_degrees = math.degrees(second_corner)
+
+            third_corner = math.acos(((a ** 2) + (c ** 2) - (b ** 2)) / (2 * a * c))
+            third_degrees = math.degrees(third_corner)
+
+            print(f"Первый угол равен: {round(first_degrees)}")
+            print(f"Первый угол равен: {round(second_degrees)}")
+            print(f"Первый угол равен: {round(third_degrees)}")
+
+            if first_degrees == 90 or second_degrees == 90 or third_degrees == 90:
+                print("Треугольник прямоугольный")
+            elif first_degrees == second_degrees or first_degrees == third_degrees or second_degrees == third_degrees:
+                print("Треугольник равнобедренный")
+            elif first_degrees == second_degrees == third_degrees:
+                print("Треугольник равносторонний")
+            else:
+                print("Обычный треугольник")
+
+            return Triangle()
+        else:
+            raise ValueError()
+
+
+if __name__ == '__main__':
+    r1 = Triangle()
+    r1.read("Введите стороны треугольника в формате (a b c): ")
+
+    # Вывод периметра треугольника
+    r2 = r1.add(r1)
+    r2.display()
+
+    # Вывод площади треугольника
+    r3 = r1.sub(r1)
+    r3.display()
+
+    # Вывод высоты треугольника к стороне A
+    r4 = r1.height_one(r1)
+    r4.display()
+
+    # Вывод высоты треугольника к стороне B
+    r5 = r1.height_two(r1)
+    r5.display()
+
+    # Вывод высоты треугольника к стороне C
+    r6 = r1.height_three(r1)
+    r6.display()
+
+    # Вывод первого угла треугольника
+    r7 = r1.corner_one(r1)
+    r7.display()
--- individ_1.py
+++ individ_1.py
@@ -1,171 +1,90 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
 import math
 
 
 #   Создать класс Triangle для представления треугольника. Поля данных должны включать
-#   углы и стороны. Требуется реализовать
-#   операции: получения полей данных,
+#   углы и стороны. Требуется реализовать операции: получения полей данных,
 #   вычисления площади, вычисления периметра, вычисления высот, а также определения
 #   вида треугольника (равносторонний, равнобедренный или прямоугольный).
 
 class Triangle:
 
-    def __init__(self, side=0):
-        side = float(side)
-        self.__first = side
-        self.__second = side
-        self.__third = side
+    def __init__(self, first=0, second=0, third=0):
+        self.first = int(first)
+        self.second = int(second)
+        self.third = int(third)
 
-    @property
-    def first(self):
-        return self.__first
-
-    @property
-    def second(self):
-        return self.__second
-
-    @property
-    def third(self):
-        return self.__third
+        self.per()
 
     # Ввод сторон треугольника
-    def read(self, prompt=None):
-        line = input() if prompt is None else input(prompt)
-        parts = list(map(float, line.split()))
-        if len(parts) != 3:
-            print("Неверный размер списка", file=sys.stderr)
-            exit(1)
+    def read(self):
+        first = input('Введите первую сторону треугольника: ')
+        second = input('Введите вторую сторону треугольника: ')
+        third = input('Введите третью сторону треугольника: ')
 
-        if parts[0] == 0 or parts[1] == 0 or parts[2] == 0:
-            raise ValueError()
-
-        if parts[0] < 1 or parts[1] < 1 or parts[2] < 1:
-            raise ValueError()
-
-        self.__first = abs(parts[0])
-        self.__second = abs(parts[1])
-        self.__third = abs(parts[2])
-
-    # Вывести числа на экран
-    def display(self):
-        print(f"{self.__first}")
+        self.first = int(first)
+        self.second = int(second)
+        self.third = int(third)
 
     # Вычисление периметра треугольника
-    def add(self, rhs):
-        if isinstance(rhs, Triangle):
-            self.perimeter = self.first + self.second + self.third
-            print(f"Периметр треугольника равен: ")
-
-            return Triangle(self.perimeter)
-        else:
-            raise ValueError()
+    def per(self):
+        return self.first + self.second + self.third
 
     # Вычисление площади треугольника
-    def sub(self, rhs):
-        if isinstance(rhs, Triangle):
-            p = self.perimeter / 2
-            square = math.sqrt(p * (p - self.first) * (p - self.second) * (p - self.third))
-            print(f"Площадь треугольника равна: ")
-
-            return Triangle(square)
-        else:
-            raise ValueError()
+    def square(self):
+        self.perimeter = self.first + self.second + self.third
+        p = self.perimeter / 2
+        return math.sqrt(p * (p - self.first) * (p - self.second) * (p - self.third))
 
     # Вычисление высоты проведенной к стороне A
-    def height_one(self, rhs):
-        if isinstance(rhs, Triangle):
-            p = self.perimeter / 2
-            h1 = 2 * math.sqrt(p * (p - self.first) * (p - self.second) * (p - self.third)) / self.first
-            print(f"Высота  проведенная к стороне A равна: ")
-
-            return Triangle(h1)
-        else:
-            raise ValueError()
+    def height_one(self):
+        p = self.perimeter / 2
+        return 2 * math.sqrt(p * (p - self.first) * (p - self.second) * (p - self.third)) / self.first
 
     # Вычисление высоты проведенной к стороне B
-    def height_two(self, rhs):
-        if isinstance(rhs, Triangle):
-            p = self.perimeter / 2
-            h2 = 2 * math.sqrt(
-                p * (p - self.first) * (p - self.second) * (p - self.third)) / self.second
-            print(f"Высота  проведенная к стороне B равна: ")
-
-            return Triangle(h2)
-        else:
-            raise ValueError()
+    def height_two(self):
+        p = self.perimeter / 2
+        return 2 * math.sqrt(p * (p - self.first) * (p - self.second) * (p - self.third)) / self.second
 
     # Вычисление высоты проведенной к стороне C
-    def height_three(self, rhs):
-        if isinstance(rhs, Triangle):
-            p = self.perimeter / 2
-            h3 = 2 * math.sqrt(
-                p * (p - self.first) * (p - self.second) * (p - self.third)) / self.third
-            print(f"Высота  проведенная к стороне C равна: ")
+    def height_three(self):
+        p = self.perimeter / 2
+        return 2 * math.sqrt(p * (p - self.first) * (p - self.second) * (p - self.third)) / self.third
 
-            return Triangle(h3)
+    # Вычисление градусов углов по формуле Герона
+    def corner_one(self):
+        a = self.first
+        b = self.second
+        c = self.third
+        first_corner = math.acos(((b ** 2) + (c ** 2) - (a ** 2)) / (2 * c * b))
+        self.f_d = math.degrees(first_corner)
+
+        second_corner = math.acos(((a ** 2) + (b ** 2) - (c ** 2)) / (2 * a * b))
+        self.s_d = math.degrees(second_corner)
+
+        third_corner = math.acos(((a ** 2) + (c ** 2) - (b ** 2)) / (2 * a * c))
+        self.th_d = math.degrees(third_corner)
+
+        if self.f_d == 90 or self.s_d == 90 or self.th_d == 90:
+            print("Треугольник прямоугольный")
+        elif self.f_d == self.s_d or self.f_d == self.th_d or self.s_d == self.th_d:
+            print("Треугольник равнобедренный")
+        elif  self.f_d == self.s_d == self.th_d:
+            print("Треугольник равносторонний")
         else:
-            raise ValueError()
-
-    # Вычисление градусов углов
-    def corner_one(self, rhs):
-        if isinstance(rhs, Triangle):
-            a = self.first
-            b = self.second
-            c = self.third
-            first_corner = math.acos(((b ** 2) + (c ** 2) - (a ** 2)) / (2 * c * b))
-            first_degrees = math.degrees(first_corner)
-
-            second_corner = math.acos(((a ** 2) + (b ** 2) - (c ** 2)) / (2 * a * b))
-            second_degrees = math.degrees(second_corner)
-
-            third_corner = math.acos(((a ** 2) + (c ** 2) - (b ** 2)) / (2 * a * c))
-            third_degrees = math.degrees(third_corner)
-
-            print(f"Первый угол равен: {round(first_degrees)}")
-            print(f"Первый угол равен: {round(second_degrees)}")
-            print(f"Первый угол равен: {round(third_degrees)}")
-
-            if first_degrees == 90 or second_degrees == 90 or third_degrees == 90:
-                print("Треугольник прямоугольный")
-            elif first_degrees == second_degrees or first_degrees == third_degrees or second_degrees == third_degrees:
-                print("Треугольник равнобедренный")
-            elif first_degrees == second_degrees == third_degrees:
-                print("Треугольник равносторонний")
-            else:
-                print("Обычный треугольник")
-
-            return Triangle()
-        else:
-            raise ValueError()
+            print("Обычный треугольник")
 
 
 if __name__ == '__main__':
     r1 = Triangle()
-    r1.read("Введите стороны треугольника в формате (a b c): ")
+    r1.read()
 
-    # Вывод периметра треугольника
-    r2 = r1.add(r1)
-    r2.display()
+    print(r1.per())
+    print(r1.square())
+    print(r1.height_one())
+    print(r1.height_two())
+    print(r1.height_three())
 
-    # Вывод площади треугольника
-    r3 = r1.sub(r1)
-    r3.display()
 
-    # Вывод высоты треугольника к стороне A
-    r4 = r1.height_one(r1)
-    r4.display()
-
-    # Вывод высоты треугольника к стороне B
-    r5 = r1.height_two(r1)
-    r5.display()
-
-    # Вывод высоты треугольника к стороне C
-    r6 = r1.height_three(r1)
-    r6.display()
-
-    # Вывод первого угла треугольника
-    r7 = r1.corner_one(r1)
-    r7.display()
